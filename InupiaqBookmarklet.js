9066c3347b1074df0d751fabe57532bb1cafd0a5
Joshua Baker <joshuakb2@gmail.com>
2018-12-11 12:51:39 -0600

    Changed my approach to try to work around bugs and security policies, but I made it even worse lol

--- InupiaqBookmarklet.js
+++ InupiaqBookmarklet.js
@@ -1,41 +1,60 @@
 (function() {
-    let target = '_self';
+    let formTarget = '_self';
+
     if (window.location.hostname + window.location.pathname != 'ankn.uaf.edu/ANL/mod/glossary/view.php') {
-        target = '_blank';
+        //  We could be anywhere. Various security policies will screw up the behavior
+        //  of this bookmarklet, and we also don't want to navigate away from whatever
+        //  the user might be doing. Open a new page and execute this script from there.
+        //
+        let beforeSearch = "data:text/html,<html><head><title>Searching...</title><script>";
+        let afterSearch = "</script></head><body>Searching...<script>formTarget='_blank';window.onload=" + search.name + ";</script></body></html>"
+
+        let URI = beforeSearch + search.toString() + afterSearch;
+
+        let iframe = document.createElement('IFRAME');
+        iframe.src = URI;
+        iframe.style.display = 'hidden';
+        document.body.appendChild(iframe);
     }
-    
-    let form = document.createElement('FORM');
-    form.style="display: none";
-    form.action = 'http://ankn.uaf.edu/ANL/mod/glossary/view.php';
-    form.target = target;form.method = 'POST';
-    
-    let textInput = document.createElement('INPUT');
-    textInput.type = 'text';
-    textInput.name = 'hook';
-    form.appendChild(textInput);
-    
-    let checkbox = document.createElement('INPUT');
-    checkbox.type = 'checkbox';
-    checkbox.name = 'fullsearch';
-    checkbox.value = 1;
-    checkbox.checked = 'checked';
-    form.appendChild(checkbox);
-    
-    let id = document.createElement('INPUT');
-    id.name = 'id';
-    id.value = 20;
-    form.appendChild(id);
-    
-    let mode = document.createElement('INPUT');
-    mode.name = 'mode';
-    mode.value = 'search';
-    form.appendChild(mode);
-    
-    let query = prompt('What Inupiaq or English word/phrase would you like to search for?');
-    
-    if (query) {
-        document.body.appendChild(form);
-        textInput.value = query;
-        form.submit();
+    else {
+        search();
+    }
+
+    function search() {
+        let form = document.createElement('FORM');
+        form.style="display: none";
+        form.action = 'http://ankn.uaf.edu/ANL/mod/glossary/view.php';
+        form.target = formTarget;
+        form.method = 'POST';
+        
+        let textInput = document.createElement('INPUT');
+        textInput.type = 'text';
+        textInput.name = 'hook';
+        form.appendChild(textInput);
+        
+        let checkbox = document.createElement('INPUT');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'fullsearch';
+        checkbox.value = 1;
+        checkbox.checked = 'checked';
+        form.appendChild(checkbox);
+        
+        let id = document.createElement('INPUT');
+        id.name = 'id';
+        id.value = 20;
+        form.appendChild(id);
+        
+        let mode = document.createElement('INPUT');
+        mode.name = 'mode';
+        mode.value = 'search';
+        form.appendChild(mode);
+        
+        let query = prompt('What Inupiaq or English word/phrase would you like to search for?');
+        
+        if (query) {
+            document.body.appendChild(form);
+            textInput.value = query;
+            form.submit();
+        }
     }
 }());
