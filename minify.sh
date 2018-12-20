23ea49f233a342129215a313383e4c875930134f
Joshua Baker <joshuakb2@gmail.com>
2018-12-20 14:27:33 -0600

    Improved minify.sh so that newlines are avoided in the output.

--- minify.sh
+++ minify.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-node minify.js
+if command -v node >/dev/null 2>&1; then
+    if node minify.js; then
+        paste -s -d '' InupiaqBookmarklet.min.js >noNewLines.js
+        mv noNewLines.js InupiaqBookmarklet.min.js
+        exit 0
+    else
+        echo "Error while minifying script."
+        echo
+        exit 2
+    fi
+else
+    echo "You must install node to run this script."
+    echo
+    exit 1
+fi
